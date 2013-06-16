cc4f0a848597431369b97abaa368e44dffaf5657
Paolo Zaccagnini <paozac@gmail.com>
2013-06-16 19:03:16 +0100

    Extract some EXIF data

--- lens-stats.py
+++ lens-stats.py
@@ -1,3 +1,6 @@
+import glob
+import PIL.Image
+
 from PIL.ExifTags import TAGS
 
 def get_exif_data(fname):
@@ -15,4 +18,14 @@ def get_exif_data(fname):
         print 'IOERROR ' + fname
     return ret
 
-print "Hello"
+def get_files():
+    "Returns an array of matching filenames"
+    files = glob.glob("**/*.jpg")
+    return files
+
+def run():
+    files = get_files()
+    for file in files:
+        print get_exif_data(file)
+
+run()
