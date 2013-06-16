e32db6c242ab553d70bab697d83a9f479be5e8c4
Paolo Zaccagnini <paozac@gmail.com>
2013-06-16 11:52:38 +0100

    Initial commit

---
+++ lens-stats.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def get_exif_data(fname):
+    "Get embedded EXIF data from image file."
+    ret = {}
+    try:
+        img = Image.open(fname)
+        if hasattr( img, '_getexif' ):
+            exifinfo = img._getexif()
+            if exifinfo != None:
+                for tag, value in exifinfo.items():
+                    decoded = TAGS.get(tag, tag)
+                    ret[decoded] = value
+    except IOError:
+        print 'IOERROR ' + fname
+    return ret
+
+print "Hello"
